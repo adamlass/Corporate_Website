c1ff9ecbb917b165587868aac525d60dcda924b2
adamlass <33089759+adamlass@users.noreply.github.com>
2018-10-13 11:54:38 +0200

    first commit

---
+++ src/main/webapp/js/linkers.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+var CA1 = document.getElementById("CA1");
+CA1.addEventListener("click",function (){
+    window.location.replace("/CA1");
+});
+
+var CA2 = document.getElementById("CA2");
+CA2.addEventListener("click",function (){
+    window.location.replace("/Krak");
+});
